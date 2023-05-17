70d76e37a54082fb1c268aec5ca7d6c39f2a0f30
KaMeR <kurowskimateusz04@gmail.com>
2023-05-17 22:46:53 +0200

    add session update DTO

---
+++ TimeGuardian_API/TimeGuardian_API/Models/DTO/SessionUpdateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace TimeGuardian_API.Models.DTO;
+
+public class SessionUpdateDTO
+{
+    public DateTime StartTime { get; set; }
+    public DateTime? EndTime { get; set; }
+    public int? Duration { get; set; }
+}

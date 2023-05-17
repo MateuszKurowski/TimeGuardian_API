646b649ec1f27d0200986377af158103d75f5937
KaMeR <kurowskimateusz04@gmail.com>
2023-05-17 22:47:00 +0200

    add user DTO

---
+++ TimeGuardian_API/TimeGuardian_API/Models/DTO/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeGuardian_API.Models.DTO;
+
+public class UserDTO
+{
+    public string Email { get; set; }
+
+    [Required]
+    public string Login { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+}

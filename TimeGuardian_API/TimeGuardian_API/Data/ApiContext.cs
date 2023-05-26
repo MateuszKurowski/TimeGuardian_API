a6b3805cc9e028b458e72fe497d94cae863c759a
KaMeR <kurowskimateusz04@gmail.com>
2023-05-26 12:45:02 +0200

    Update Database Seed

--- TimeGuardian_API/TimeGuardian_API/Data/ApiContext.cs
+++ TimeGuardian_API/TimeGuardian_API/Data/ApiContext.cs
@@ -30,7 +30,22 @@ public class ApiContext : DbContext
                    Password = "Admin1",
                    Email = "admin@admin.com",
                    CreatedAt = DateTime.Now
+               },
+               new User
+               {
+                   Id = 2,
+                   Login = "Jan",
+                   Password = "Jan1",
+                   Email = "jan@jan.com",
+                   CreatedAt = DateTime.Now
                }
+            );
+
+        modelBuilder.Entity<Session>().HasData(
+            new Session { Id = 1, UserId = 1, SessionTypeId = 1, StartTime = new DateTime(2023, 5, 17, 17, 12, 34), EndTime = new DateTime(2023, 5, 17, 18, 58, 13), Duration = 6339 },
+            new Session { Id = 2, UserId = 1, SessionTypeId = 3, StartTime = new DateTime(2023, 5, 17, 18, 58, 13), EndTime = new DateTime(2023, 5, 17, 19, 58, 13), Duration = 3600 },
+            new Session { Id = 3, UserId = 1, SessionTypeId = 2, StartTime = new DateTime(2023, 5, 17, 19, 23, 56), EndTime = new DateTime(2023, 5, 17, 23, 47, 3), Duration = 15787 },
+            new Session { Id = 4, UserId = 2, SessionTypeId = 1, StartTime = new DateTime(2023, 5, 17, 19, 23, 56), EndTime = new DateTime(2023, 5, 17, 23, 47, 3), Duration = 15787 }
             );
     }
 }

3bde675b77370b9d6e2bae7134055f8d5a22d3f2
KaMeR <kurowskimateusz04@gmail.com>
2023-05-26 12:44:50 +0200

    Add new endpoint to SessionController

--- TimeGuardian_API/TimeGuardian_API/Models/DTO/SessionUpdateDTO.cs
+++ TimeGuardian_API/TimeGuardian_API/Models/DTO/SessionUpdateDTO.cs
@@ -1,8 +1,13 @@
-﻿namespace TimeGuardian_API.Models.DTO;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeGuardian_API.Models.DTO;
 
 public class SessionUpdateDTO
 {
     public DateTime StartTime { get; set; }
     public DateTime? EndTime { get; set; }
     public int? Duration { get; set; }
+
+    [Required]
+    public int SessionTypeId { get; set; }
 }
